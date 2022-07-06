bb1a53b4f3c4e274f7f88bbe17111139a23e4f3d
5475475r75 <dhananibrijesh26@gmail.com>
2022-07-06 13:27:05 +0530

    Contact update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import Footer from "./Component/Footer";
 import Header from "./Component/Header";
 import Home from "./Container/Home";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import Contact from "./Container/Contact";
 
 
 function App() {
     return (
-      <div className="App">
-        <Header/>
-        <Home />
-        <Footer />
-      </div>
+      <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Header />} >
+          <Route exact path='/' element={<Footer />} >
+            <Route index element={<Home />} />
+            <Route exact path='contact' element={<Contact />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
     );
 }
 
---
+++ src/Container/Contact.js
@@ -0,0 +1,250 @@
+import React from 'react';
+
+function Contact(props) {
+    return (
+        <>
+            <main>
+                {/* page-banner-area-start */}
+                <div className="page-banner-area page-banner-height" data-background="assets/img/banner/page-banner-3.jpg">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xl-12">
+                                <div className="page-banner-content text-center">
+                                    <h4 className="breadcrumb-title">Contact Us</h4>
+                                    <div className="breadcrumb-two">
+                                        <nav>
+                                            <nav className="breadcrumb-trail breadcrumbs">
+                                                <ul className="breadcrumb-menu">
+                                                    <li className="breadcrumb-trail">
+                                                        <a href="index.html"><span>Home</span></a>
+                                                    </li>
+                                                    <li className="trail-item">
+                                                        <span>Contact Us</span>
+                                                    </li>
+                                                </ul>
+                                            </nav>
+                                        </nav>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* page-banner-area-end */}
+                {/* location-area-start */}
+                <div className="location-area pt-70 pb-25">
+                    <div className="container">
+                        <div className="row mb-25">
+                            <div className="col-xl-12">
+                                <div className="abs-section-title text-center">
+                                    <span>LOCATION STORE</span>
+                                    <h4>Where We Are</h4>
+                                    <p>The perfect way to enjoy brewing tea on low hanging fruit to identify. Duis autem vel eum iriure dolor in hendrerit <br /> in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-xl-3 col-lg-3 col-md-6">
+                                <div className="location-item mb-30">
+                                    <div className="location-image w-img mb-20">
+                                        <img src="assets/img/location/location-1.jpg" alt />
+                                    </div>
+                                    <h6>26 Rue Pelleport - Paris</h6>
+                                    <div className="sm-item-loc sm-item-border mb-20">
+                                        <div className="sml-icon mr-20">
+                                            <i className="fal fa-map-marker-alt" />
+                                        </div>
+                                        <div className="sm-content">
+                                            <span>Find us</span>
+                                            <p>Rue Saint-Antoine, Paris, France</p>
+                                        </div>
+                                    </div>
+                                    <div className="sm-item-loc sm-item-border mb-20">
+                                        <div className="sml-icon mr-20">
+                                            <i className="fal fa-phone-alt" />
+                                        </div>
+                                        <div className="sm-content">
+                                            <span>Call us</span>
+                                            <p><a href="tel:+8804568">(+100) 123 456 7890</a></p>
+                                        </div>
+                                    </div>
+                                    <div className="sm-item-loc mb-20">
+                                        <div className="sml-icon mr-20">
+                                            <i className="fal fa-envelope" />
+                                        </div>
+                                        <div className="sm-content">
+                                            <span>Mail us</span>
+                                            <p><a href="mailto:store@company.com">store@company.com</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xl-3 col-lg-3 col-md-6">
+                                <div className="location-item mb-30">
+                                    <div className="location-image w-img mb-20">
+                                        <img src="assets/img/location/location-2.jpg" alt />
+                                    </div>
+                                    <h6>150 Stanley Rd - London</h6>
+                                    <div className="sm-item-loc sm-item-border mb-20">
+                                        <div className="sml-icon mr-20">
+                                            <i className="fal fa-map-marker-alt" />
+                                        </div>
+                                        <div className="sm-content">
+                                            <span>Find us</span>
+                                            <p>Brick Ln, Spitalfields, London E1, UK</p>
+                                        </div>
+                                    </div>
+                                    <div className="sm-item-loc sm-item-border mb-20">
+                                        <div className="sml-icon mr-20">
+                                            <i className="fal fa-phone-alt" />
+                                        </div>
+                                        <div className="sm-content">
+                                            <span>Call us</span>
+                                            <p><a href="tel:+8804568">(+100) 123 456 7890</a></p>
+                                        </div>
+                                    </div>
+                                    <div className="sm-item-loc mb-20">
+                                        <div className="sml-icon mr-20">
+                                            <i className="fal fa-envelope" />
+                                        </div>
+                                        <div className="sm-content">
+                                            <span>Mail us</span>
+                                            <p><a href="mailto:store@company.com">store@company.com</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xl-3 col-lg-3 col-md-6">
+                                <div className="location-item mb-30">
+                                    <div className="location-image w-img mb-20">
+                                        <img src="assets/img/location/location-3.jpg" alt />
+                                    </div>
+                                    <h6>1357 Prospect - New York</h6>
+                                    <div className="sm-item-loc sm-item-border mb-20">
+                                        <div className="sml-icon mr-20">
+                                            <i className="fal fa-map-marker-alt" />
+                                        </div>
+                                        <div className="sm-content">
+                                            <span>Find us</span>
+                                            <p>Atlantic, Brooklyn, New York, US</p>
+                                        </div>
+                                    </div>
+                                    <div className="sm-item-loc sm-item-border mb-20">
+                                        <div className="sml-icon mr-20">
+                                            <i className="fal fa-phone-alt" />
+                                        </div>
+                                        <div className="sm-content">
+                                            <span>Call us</span>
+                                            <p><a href="tel:+8804568">(+100) 123 456 7890</a></p>
+                                        </div>
+                                    </div>
+                                    <div className="sm-item-loc mb-20">
+                                        <div className="sml-icon mr-20">
+                                            <i className="fal fa-envelope" />
+                                        </div>
+                                        <div className="sm-content">
+                                            <span>Mail us</span>
+                                            <p><a href="mailto:store@company.com">store@company.com</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xl-3 col-lg-3 col-md-6">
+                                <div className="location-item mb-30">
+                                    <div className="location-image w-img mb-20">
+                                        <img src="assets/img/location/location-4.jpg" alt />
+                                    </div>
+                                    <h6>86 Georgia Rd - Mardrid</h6>
+                                    <div className="sm-item-loc sm-item-border mb-20">
+                                        <div className="sml-icon mr-20">
+                                            <i className="fal fa-map-marker-alt" />
+                                        </div>
+                                        <div className="sm-content">
+                                            <span>Find us</span>
+                                            <p>Calle del Correo, 4, Madrid, Spain</p>
+                                        </div>
+                                    </div>
+                                    <div className="sm-item-loc sm-item-border mb-20">
+                                        <div className="sml-icon mr-20">
+                                            <i className="fal fa-phone-alt" />
+                                        </div>
+                                        <div className="sm-content">
+                                            <span>Call us</span>
+                                            <p><a href="tel:+8804568">(+100) 123 456 7890</a></p>
+                                        </div>
+                                    </div>
+                                    <div className="sm-item-loc mb-20">
+                                        <div className="sml-icon mr-20">
+                                            <i className="fal fa-envelope" />
+                                        </div>
+                                        <div className="sm-content">
+                                            <span>Mail us</span>
+                                            <p><a href="mailto:store@company.com">store@company.com</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* location-area-end */}
+                {/* cmamps-area-start */}
+                <div className="cmamps-area">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1899531.5831083965!2d105.806381!3d21.58504!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x515f4860ede9e108!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBDw7RuZyBuZ2jhu4cgVGjDtG5nIHRpbiB2w6AgVHJ1eeG7gW4gdGjDtG5n!5e0!3m2!1sen!2sus!4v1644226635446!5m2!1sen!2sus" />
+                </div>
+                {/* cmamps-area-end */}
+                {/* cta-area-start */}
+                <section className="cta-area d-ldark-bg pt-55 pb-10">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-4 col-md-6">
+                                <div className="cta-item cta-item-d mb-30">
+                                    <h5 className="cta-title">Follow Us</h5>
+                                    <p>We make consolidating, marketing and tracking your social media website easy.</p>
+                                    <div className="cta-social">
+                                        <div className="social-icon">
+                                            <a href="#" className="facebook"><i className="fab fa-facebook-f" /></a>
+                                            <a href="#" className="twitter"><i className="fab fa-twitter" /></a>
+                                            <a href="#" className="youtube"><i className="fab fa-youtube" /></a>
+                                            <a href="#" className="linkedin"><i className="fab fa-linkedin-in" /></a>
+                                            <a href="#" className="rss"><i className="fas fa-rss" /></a>
+                                            <a href="#" className="dribbble"><i className="fab fa-dribbble" /></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-4 col-md-6">
+                                <div className="cta-item mb-30">
+                                    <h5 className="cta-title">Sign Up To Newsletter</h5>
+                                    <p>Join 60.000+ subscribers and get a new discount coupon  on every Saturday.</p>
+                                    <div className="subscribe__form">
+                                        <form action="#">
+                                            <input type="email" placeholder="Enter your email here..." />
+                                            <button>subscribe</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-4 col-md-6">
+                                <div className="cta-item mb-30">
+                                    <h5 className="cta-title">Download App</h5>
+                                    <p>DukaMarket App is now available on App Store &amp; Google Play. Get it now.</p>
+                                    <div className="cta-apps">
+                                        <div className="apps-store">
+                                            <a href="#"><img src="assets/img/brand/app_ios.png" alt /></a>
+                                            <a href="#"><img src="assets/img/brand/app_android.png" alt /></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                {/* cta-area-end */}
+            </main>
+
+        </>
+    );
+}
+
+export default Contact;
